3e86404bf5d4d21d8a03b7579755d0695f4f975f
Erik Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2024-11-22 21:07:42 -0500

    Add files via upload

---
+++ my-health-pal/src/test/java/com/my_health_pal/MyHealthPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_health_pal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHealthPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
