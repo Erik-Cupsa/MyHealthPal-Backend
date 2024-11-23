c1ebfc9ee31f6076cbd3e88b39d08f92315d242b
YassineMimet1 <yassine.mimet@mail.mcgill.ca>
2024-11-22 23:46:20 -0500

    Added session type for both doctor and mentalHealthTherapist

--- my-health-pal/src/main/java/com/my_health_pal/model/Session.java
+++ my-health-pal/src/main/java/com/my_health_pal/model/Session.java
@@ -1,14 +1,25 @@
 package com.my_health_pal.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.my_health_pal.enums.SessionType;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "sessions")
@@ -33,6 +44,9 @@ public class Session {
 
     @Column(nullable = false)
     private Boolean completed = false;
+
+    @Column(nullable = false)
+    private SessionType sessionType;
 
     @OneToMany(mappedBy = "session", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> messages = new ArrayList<>();
